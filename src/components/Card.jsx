dcda662b9ba5816a8111b01569556fa3f051252f
Nassy Kova <mailtosummer@gmail.com>
2023-07-03 09:50:02 +1000

    Add openSpots and Online badge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,14 @@
 export default function Card(props) {
+    let badgeText;
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE";
+    }
+
     return (
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img
                 src={`./images/${props.img}`}
                 alt="swim"
