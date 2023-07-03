bd6329585d09d1c6ef43ab682dec3f834c20fb1b
Nassy Kova <mailtosummer@gmail.com>
2023-07-03 10:07:13 +1000

    Restructrure props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 export default function Card(props) {
     let badgeText;
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT";
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE";
     }
 
@@ -10,7 +10,7 @@ export default function Card(props) {
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
             <img
-                src={`./images/${props.img}`}
+                src={`./images/${props.item.coverImg}`}
                 alt="swim"
                 className="card--img"
             />
@@ -20,13 +20,15 @@ export default function Card(props) {
                     alt="star"
                     className="card--star"
                 ></img>
-                <span>{props.rating}</span>
-                <span className="grey">({props.reviewCount}) • </span>
-                <span className="grey">{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="grey">
+                    ({props.item.stats.reviewCount}) •{" "}
+                </span>
+                <span className="grey">{props.item.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
+            <p className="card--title">{props.item.title}</p>
             <p className="card--price">
-                <span className="bold">From {props.price}$</span> / person
+                <span className="bold">From {props.item.price}$</span> / person
             </p>
         </div>
     );
